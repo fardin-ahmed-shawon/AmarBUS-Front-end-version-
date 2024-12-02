b04be01484033ab01f5eb849c75fcf3d4722c509
Fardin Ahmed Shawon <65533305+fardin-ahmed-shawon@users.noreply.github.com>
2024-12-02 22:51:18 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementById("sidebar");
+
+function profile_open() {
+    sidebar.style.display = "block";
+}
+function profile_close() {
+    sidebar.style.display = "none";
+}
